9a748483bd6b1091d4bf51ea4b64ae71f37b625b
Angelo <angelomilienjoseph@gmail.com>
2021-06-08 18:58:14 -0400

    working on index.rb

--- app/controllers/lakewood_stops_controller.rb
+++ app/controllers/lakewood_stops_controller.rb
@@ -1,22 +1,15 @@
 class LakewoodStopsController < ApplicationController
-    
-    # get "/route/:route/stops" do
-    #     @route = Route.find(params[:route])
-    #     @stops = @route.stops
-    #     erb :'routes/show', :layout => false
-    # end
 
     get "/stops/new" do
         erb :'stops/new', layout: false
     end
 
     post '/stops/new' do
-        puts params
-        # @route = Route.new(params[:route])
-        # @stop = Stop.new{}
-        # erb :'stops/index', :layout => false
+        @route = Route.create(params[:route])
+        @stop = Stop.create(params[:stop])
+        erb :'stops/index', :layout => false
     end
 end
 
-# {"route"=>{"route_number"=>"15"}, "stop_name"=>{"stop_name"=>"Hollies Deli"}, 
-# "stop_address"=>{"stop_address"=>"226 Hollies Blvd"}, "phone_number"=>{"phone_number"=>"N.A"}}
+# {"route"=>{"route_number"=>"15"}, 
+# "stop"=>{"name"=>"Merrick Deli", "addresse"=>"asdsdfd", "phone_number"=>"N.A"}}
