668623da3ea17d80b3ad155a0ae0ea9f69d89e23
Angelo <angelomilienjoseph@gmail.com>
2021-06-09 15:26:54 -0400

    erase index file form stops folder

--- app/controllers/lakewood_stops_controller.rb
+++ app/controllers/lakewood_stops_controller.rb
@@ -5,6 +5,9 @@ class LakewoodStopsController < ApplicationController
     end
 
     post '/stops/new' do
+        if !logged_in?
+            redirect "/"
+        end
         @route = Route.find_or_create_by(params[:route])
         @stop = Stop.create(params[:stop])
         @route.stops << @stop
@@ -19,20 +22,22 @@ class LakewoodStopsController < ApplicationController
         @user = current_user
         @stop = Stop.find(params[:stop])
         erb :'users/show', :layout => false
-
     end
 
     get '/edit/:user_id/:stop_id' do
         "hello"
     end
-end
 
-# {"route"=>{"route_number"=>"15"}, 
-# "stop"=>{"name"=>"Merrick Deli", "addresse"=>"asdsdfd", "phone_number"=>"N.A"}}
-# <% @routes.each do |route| %>
-#   <button class="button_5">
-#      <a href="/route/<%= route.id %>/stops">
-#         <%= route.route_number %>
-#      </a>
-#   </button>
-#  <% end %>
+
+#   patch "/articles/:id" do
+#     @article = Article.find(params[:id])
+#     params[:article]
+#     @article.update(params[:article])
+#     redirect to "/articles/#{ @article.id }"
+#   end
+
+#   delete "/articles/:id" do
+#     Article.destroy(params[:id])
+#     redirect to "/articles"
+#   end
+end
